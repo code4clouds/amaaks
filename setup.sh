8d75858d449ae350825e74132c9dc0e1528bb1a8
Julio Colon <jucolon@microsoft.com>
2020-10-17 19:44:08 -0400

    added kube install

--- setup.sh
+++ setup.sh
@@ -79,6 +79,15 @@ sudo cat > /etc/docker/daemon.json <<-EOF
 }
 EOF
 sudo mv daemon.json /etc/docker/daemon.json
+cd ..
+
+# Install KubeCtl
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 
 exit;
 # Set cron to assure all Docker-compose service are up
